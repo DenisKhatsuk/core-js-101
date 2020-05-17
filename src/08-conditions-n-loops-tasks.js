d1bbb07ca71bbb3018320f86967dac3667d5a690
Denis Khatsuk <dennis.hatsuk@gmail.com>
2020-05-17 22:00:39 +0300

    feat: 51 tasks done

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -283,8 +283,14 @@ function isCreditCardNumber(/* ccn */) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  let sum = 0;
+  const numberStr = num.toString();
+  for (let i = 0; i < numberStr.length; i += 1) {
+    sum += +numberStr[i];
+  }
+  sum = sum > 9 ? getDigitalRoot(sum) : sum;
+  return sum;
 }
 
 
